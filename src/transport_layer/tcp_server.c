8ace813dd40e9104ad18f06f29d3c7845e8086c6
William Brumble <W.Brumble@gmail.com>
2020-12-21 23:25:58 -0600

    upload

--- src/transport_layer/tcp_server.c
+++ src/transport_layer/tcp_server.c
@@ -1,42 +1,93 @@
-//
-// Created by william on 12/17/20.
-//
-
 #include <stdio.h>
 #include "tcp_server.h"
 #include <sys/socket.h>
 #include <sys/epoll.h>
 #include <netinet/in.h>
+#include <limits.h>
 
 // ----------------- Configuration Options
-#define DOMAIN AF_INET         // The domain argument specifies a communication domain.
-#define TYPE SOCK_STREAM       // Specifies the communication semantics.
-#define PROTOCOL 6             // Specifies a particular protocol to be used with the socket.
+#define DOMAIN AF_INET  // The domain argument specifies a communication domain.
+#define TYPE SOCK_STREAM    // Specifies the communication semantics.
+#define PROTOCOL 6  // Specifies a particular protocol to be used with the socket.
 
-// int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-// int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-// int listen(int sockfd, int backlog);
-// int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-// epoll_create1(2)
-// epoll_ctl(2)
-// epoll_wait(2)
-// send(2), sendto(2), and sendmsg(2)
-// recv(2), recvfrom(2), recvmsg(2)
-// int close(int fd);
+int main (){
+    int i;  // Used as an iterator.
+    int maxi = 0;   // Contains the largest index of the client array currently in use.
+    int listenfd; //
+    int connfd; // Used to store an accepted client.
+    int sockfd; // Used to store a clients fd.
+    int nready; // Used to store the number of elements in pollfds that have events or errors.
+    ssize_t n; // Used to store count of bytes received from client.
+    char buf[MAXLINE]; // Buffer to store incoming data.
+    socklen_t clilen; // Because sockets _needs_ it's own integer type.
+    struct pollfd client[OPEN_MAX]; // Used to store all of the connections fds.
+    struct sockaddr_in clienaddr; // Used to store the connected client's internet address.
+    struct sockaddr_in serveraddr; // Used to store our internet address.
 
-int main(){
-	// -------- Setup the tcp socket.
-	int tcp_socket = socket(DOMAIN, TYPE, PROTOCOL); // Creates an endpoint for communication.
+    // -------- Create the TCP socket.
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	// -------- Setup the socket address.
-	struct sockaddr_in tcp_address;			// Used to define a socket address.
-	tcp_address.sin_family = DOMAIN;		// Address family.
-	tcp_address.sin_port = htons(0xAF12);		// Port number.
-	tcp_address.sin_addr.s_addr = INADDR_ANY;	// Internet address.
-	
-	// -------- Assign the address to the socket.	
-	bind(tcp_socket, (struct sockaddr*)&tcp_socket, sizeof(tcp_socket));
+    // -------- Create the address for the TCP socket.
+    bzero(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERV_PORT);
 
-	printf("The program has completed\n");
-	return 0;
+    // -------- Assign the address to the TCP socket.
+    bind(listenfd, (SA *) &serveraddr, sizeof(serveraddr));
+
+    // -------- Setup the TCP socket for listening.
+    listen(listenfd, LISTENQ);
+
+    // -------- Set the first element in the client array to the TCP socket.
+    client[0].fd = listenfd;
+    client[0].events = POLLRDNORM;
+
+    // -------- Set all of the other elements in the array as available (-1).
+    for(i = 1; i < OPEN_MAX; i++)
+        client[i].fd = -1;
+
+    // Begin the infinite loop.
+    for ( ;; ){
+
+        nready = poll(client, maxi + 1, INFTIM);
+
+        if (client[0].revents & POLLRDNORM){
+            clilen = sizeof(clienaddr);
+            connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
+
+            for (i = 1; i < OPEN_MAX; i++)
+                if (client[i].fd < 0) {
+                    client[i].fd = connfd;
+                    break;
+                }
+            if (i == OPEN_MAX)
+                err_quit("Too many clients");
+            client[i].events = POLLRDNORM;
+            if (i > maxi)
+                maxi = i;
+            if (--nread <= 0)
+                continue;
+        }
+
+        for (i = 1; i <= maxi; i++) {
+            if ((sockfd = client[i].fd) < 0)
+                continue;
+            if (client[i].revents & (POLLRDNORM | POLLERR)) {
+                if ((n = read(sockfd, buf, MAXLINE)) < 0) {
+                    if (errno == ECONNRESET) {
+                        close(sockfd);
+                        client[i].fd = -1;
+                    } else
+                        err_sys("Read error");
+                } else if (n == 0) {
+                    close(sockfd);
+                    client[i].fd = -1;
+                } else
+                    writen(sockfd, buf, n);
+                if (--nready <= 0 )
+                    break;
+            }
+        }
+    }
 }
