4dc16c4df24c367070897feea188b32241c202b1
William Brumble <w.brumble@gmail.com>
2020-12-23 05:05:56 -0600

    Work has been done

--- src/transport_layer/tcp_server.c
+++ src/transport_layer/tcp_server.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <signal.h>
 #include "tcp_server.h"
 #include <sys/socket.h>
-#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <limits.h>
+#include <errno.h>
 
 // ----------------- Configuration Options
 #define DOMAIN AF_INET  // The domain argument specifies a communication domain.
 #define TYPE SOCK_STREAM    // Specifies the communication semantics.
 #define PROTOCOL 6  // Specifies a particular protocol to be used with the socket.
+#define SERV_PORT 0xAF12 // Specifies port number for socket address.
+#define LISTENQ 128 // Max Backlog Value. /proc/sys/net/core/somaxconn.
+#define MAXLINE 4096 // Max text line length.
+#define INFTIM (-1) //  Infinite poll timeout.
 
 int main (){
     int i;  // Used as an iterator.
@@ -20,71 +27,102 @@ int main (){
     ssize_t n; // Used to store count of bytes received from client.
     char buf[MAXLINE]; // Buffer to store incoming data.
     socklen_t clilen; // Because sockets _needs_ it's own integer type.
-    struct pollfd client[OPEN_MAX]; // Used to store all of the connections fds.
+    struct pollfd client[FOPEN_MAX]; // Used to store all of the connections fds.
     struct sockaddr_in clienaddr; // Used to store the connected client's internet address.
     struct sockaddr_in serveraddr; // Used to store our internet address.
 
-    // -------- Create the TCP socket.
+    // Create the TCP socket.
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // -------- Create the address for the TCP socket.
+    // Create the address for the TCP socket.
     bzero(&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(SERV_PORT);
 
-    // -------- Assign the address to the TCP socket.
-    bind(listenfd, (SA *) &serveraddr, sizeof(serveraddr));
+    // Assign the address to the TCP socket.
+    bind(listenfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
 
-    // -------- Setup the TCP socket for listening.
+    // Setup the TCP socket for listening.
     listen(listenfd, LISTENQ);
 
-    // -------- Set the first element in the client array to the TCP socket.
+    // Set the first element in the client array to the TCP socket.
     client[0].fd = listenfd;
-    client[0].events = POLLRDNORM;
+    client[0].events = POLLIN;
 
-    // -------- Set all of the other elements in the array as available (-1).
-    for(i = 1; i < OPEN_MAX; i++)
+    // Set all of the other elements in the array as available (-1).
+    for(i = 1; i < FOPEN_MAX; i++)
         client[i].fd = -1;
 
     // Begin the infinite loop.
     for ( ;; ){
-
+        
+        // Get the number of fds that currently have an event.
         nready = poll(client, maxi + 1, INFTIM);
 
-        if (client[0].revents & POLLRDNORM){
-            clilen = sizeof(clienaddr);
-            connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
+        // If we have received a new connection.
+        if (client[0].revents & POLLIN){
 
-            for (i = 1; i < OPEN_MAX; i++)
+            // Accept the connection and store inside client struct.
+            clilen = sizeof(clienaddr);
+            connfd = accept(listenfd, (struct sockaddr *) &clienaddr, &clilen);
+
+            // Scan for an fds that's not being used.
+            for (i = 1; i < FOPEN_MAX; i++)
+                // If the fds has been set as available (-1).
                 if (client[i].fd < 0) {
+                    // Use this fds to store the connection.
                     client[i].fd = connfd;
                     break;
                 }
-            if (i == OPEN_MAX)
+            
+            // If we have reached the end of our array quit on error.
+            if (i == FOPEN_MAX)
                 err_quit("Too many clients");
-            client[i].events = POLLRDNORM;
+            client[i].events = POLLIN;
+
+            // Add current client to variable storing amount of clients.
             if (i > maxi)
                 maxi = i;
-            if (--nread <= 0)
+
+            // If there are no client fds ready, get out of the loop.
+            if (--nready <= 0)
                 continue;
         }
 
+        // Let's iterate of every client fds and service.
         for (i = 1; i <= maxi; i++) {
+
+            // If client has been set to availble (-1) skip.
             if ((sockfd = client[i].fd) < 0)
                 continue;
-            if (client[i].revents & (POLLRDNORM | POLLERR)) {
+
+            // If there is either a data or error event.
+            if (client[i].revents & (POLLIN | POLLERR)) {
+
+                // If the number of bytes read is less than 0.
                 if ((n = read(sockfd, buf, MAXLINE)) < 0) {
+
+                    // If the error equals connection reset.
                     if (errno == ECONNRESET) {
+                        // Close the socket and set the fds to available.
                         close(sockfd);
                         client[i].fd = -1;
+
+                    // Log all other errors as read error.
                     } else
                         err_sys("Read error");
+
+                // If zero bytes have been read, close the socket, and set fds as available.
                 } else if (n == 0) {
                     close(sockfd);
                     client[i].fd = -1;
+                
+                // Echo data back to client.
                 } else
-                    writen(sockfd, buf, n);
+                    write(sockfd, buf, n);
+
+                // If all clients have been serviced
                 if (--nready <= 0 )
                     break;
             }
